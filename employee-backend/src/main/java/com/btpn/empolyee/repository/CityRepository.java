4a0b1dfff64c7a1b64360ed50da5a5b7a4a6a308
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-15 22:56:47 +0700

    adding controller and Dao

--- employee-backend/src/main/java/com/btpn/empolyee/repository/CityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.btpn.empolyee.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.btpn.empolyee.domain.City;
-
-public interface CityRepository extends CrudRepository<City, String> {
-
-}
--- employee-backend/src/main/java/com/btpn/empolyee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.btpn.empolyee.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.btpn.empolyee.domain.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, String> {
-
-}
