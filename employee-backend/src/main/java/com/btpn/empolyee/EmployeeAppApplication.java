2b72bab5a8482ec963051622d13c0ddc87a2e531
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-15 13:51:18 +0700

    Initial commit

---
+++ employee-backend/src/main/java/com/btpn/empolyee/EmployeeAppApplication.java
@@ -0,0 +1,12 @@
+package com.btpn.empolyee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAppApplication.class, args);
+	}
+}
---
+++ employee-backend/src/test/java/com/btpn/empolyee/EmployeeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.btpn.empolyee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
