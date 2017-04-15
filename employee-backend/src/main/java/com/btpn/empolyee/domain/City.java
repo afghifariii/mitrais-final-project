ceff6c44c36a323282e9c7a23313341024854dfd
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-15 16:38:32 +0700

    add city domain

---
+++ employee-backend/src/main/java/com/btpn/empolyee/domain/City.java
@@ -0,0 +1,40 @@
+package com.btpn.empolyee.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class City {
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+	
+	@NotNull
+	@Column(nullable = false)
+	private String city;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
