a016e8c0c6f7404868d6c5cab44f5cab2909dcf2
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-18 20:55:08 +0700

    add controller

--- employee-backend/src/main/java/com/btpn/empolyee/controller/CityController.java
+++ employee-backend/src/main/java/com/btpn/empolyee/controller/CityController.java
@@ -1,8 +1,23 @@
 package com.btpn.empolyee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.btpn.empolyee.dao.LocationDao;
+import com.btpn.empolyee.entity.Location;
 
 @RestController
 public class CityController {
 
+	@Autowired
+	private LocationDao locationDao;
+	
+	@GetMapping("/locations/")
+	@ResponseStatus(HttpStatus.OK)
+	public Iterable<Location> allLocations() {
+		return locationDao.findAll();
+	}
 }
--- employee-backend/src/main/java/com/btpn/empolyee/controller/EmployeeController.java
+++ employee-backend/src/main/java/com/btpn/empolyee/controller/EmployeeController.java
@@ -1,8 +1,33 @@
 package com.btpn.empolyee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.btpn.empolyee.dao.EmployeeDao;
+import com.btpn.empolyee.entity.Employee;
 
 @RestController
 public class EmployeeController {
+
+	@Autowired
+	private EmployeeDao employeeDao;
+
+	@GetMapping("/employees/")
+	@ResponseStatus(HttpStatus.OK)
+	public Iterable<Employee> allEmployees() {
+
+		return employeeDao.findAll();
+	}
+	
+	@PostMapping("/employees/")
+	@ResponseStatus(HttpStatus.CREATED)
+	public void saveEmployee(@RequestBody Employee emp){
+		employeeDao.save(emp);
+	}
 
 }
