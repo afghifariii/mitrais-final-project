45aadc776d2f88e18c71c0df2a91c41d8504e366
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-18 23:52:43 +0700

    update scheme

--- employee-backend/src/main/resources/db/migration/V1.0.0.2__Data Awal.sql
+++ employee-backend/src/main/resources/db/migration/V1.0.0.2__Data Awal.sql
@@ -4,6 +4,6 @@ INSERT INTO T_LOCATION VALUES('3', 'Yogyakarta');
 INSERT INTO T_LOCATION VALUES('4', 'Bali');
 
 INSERT INTO T_EMPLOYEE
-(emp_id, first_name, last_name, gender, email, date_of_birth, nationality, loc_id)
+(emp_id, status, phone, hired_date, martial_status, sub_division, grade, division, first_name, last_name, gender, email, date_of_birth, nationality, loc_id)
 VALUES
-('1', 'Ali', 'Ghifari', 'male', 'aasdf@gmail.com','1993-06-27', 'Indonesian', '1');
+('1', 'Contract', '08113151501', '2017-03-13', 'Single', 'Java Bootcamp', 'SE-PG', 'CDC AsteRx', 'Ali', 'Ghifari', 'male', 'aasdf@gmail.com','1993-06-27', 'Indonesian', '1');
