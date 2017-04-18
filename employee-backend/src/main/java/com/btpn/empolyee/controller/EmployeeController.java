250f4b323f23f3fd6adaa8279c031747a4273f2f
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-19 00:01:39 +0700

    implement CRUD

--- employee-backend/src/main/java/com/btpn/empolyee/controller/EmployeeController.java
+++ employee-backend/src/main/java/com/btpn/empolyee/controller/EmployeeController.java
@@ -2,8 +2,11 @@ package com.btpn.empolyee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,17 +20,30 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeDao employeeDao;
 
+	@GetMapping("/employees/")
+	@ResponseStatus(HttpStatus.OK)
+	public Iterable<Employee> allEmployees() {
+
+		return employeeDao.findAll();
+	}
+	
 	@PostMapping("/employees/")
 	@ResponseStatus(HttpStatus.CREATED)
 	public void saveEmployee(@RequestBody Employee emp){
 		employeeDao.save(emp);
 	}
 	
-	@GetMapping("/employees/")
+	@PutMapping("/employees/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public Iterable<Employee> allEmployees() {
-
-		return employeeDao.findAll();
+	public void updateEmployee(@PathVariable("id") String id, @RequestBody Employee emp ){
+		emp.setEmpId(id);
+		employeeDao.save(emp);
+	}
+	
+	@DeleteMapping("employees/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deleteEmployee(@PathVariable("id") String id){
+		employeeDao.delete(id);
 	}
 
 }
