00a49219f18136ac899b9f783b13791a3a7e726b
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-20 23:19:41 +0700

    add employee model

---
+++ employee-frontend/src/app/employee/employee.model.ts
@@ -0,0 +1,25 @@
+import { Location } from '../location/location.model';
+
+export class Employee {
+    constructor(
+        public empId: string,
+        public firstName: string,
+        public lastName: string,
+        public gender: string,
+        public dateOfBirth: Date,
+        public nationality: string,
+        public martialStatus: string,
+        public phone: string,
+        public email: string,
+        public hiredDate: Date,
+        public suspendDate: Date,
+        public division: string,
+        public grade: string,
+        public subDivision: string,
+        public status: string,
+        public photo: string,
+        public location: Location
+    ) {
+
+    }
+}
