41f4602fa9489c8c15171204937d96e57baa7039
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-20 23:18:56 +0700

    add @RestResource

--- employee-backend/src/main/java/com/btpn/empolyee/dao/EmployeeDao.java
+++ employee-backend/src/main/java/com/btpn/empolyee/dao/EmployeeDao.java
@@ -1,9 +1,11 @@
 package com.btpn.empolyee.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.btpn.empolyee.entity.Employee;
 
+@RestResource(exported = false)
 public interface EmployeeDao extends PagingAndSortingRepository<Employee, String> {
 
 }
