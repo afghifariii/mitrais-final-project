4857e314d35da6913fda20ea0a21289f4990b31e
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-20 23:21:36 +0700

    configure import

--- employee-frontend/src/app/app.component.ts
+++ employee-frontend/src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('state', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in <=> out', animate('200ms ease-in-out'))
+    ]),
+    trigger('listState', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(-50%, 0, 0)'
+      })),
+      transition('in <=> out', animate('200ms ease-in-out'))
+    ]),
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  showForm = 'out';
+  formCol = 0;
+
+  toggle() {
+    this.formCol = 0;
+  };
+
+  toggleForm() {
+    this.showForm = (this.showForm === 'in' ? 'out' : 'in');
+  }
 }
--- employee-frontend/src/app/app.module.ts
+++ employee-frontend/src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MdToolbarModule, MdGridListModule, MdButtonModule, MdIconModule, MdCardModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { LocationComponent } from './location/location.component';
+
+import { EmployeeService } from "./employee/employee.service";
+import { LocationService } from "./location/location.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdToolbarModule,
+    MdGridListModule,
+    MdButtonModule,
+    MdIconModule,
+    BrowserAnimationsModule,
+    MdCardModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [EmployeeService, LocationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
