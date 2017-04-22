555a836568f33f472cbbcf58da035c744751c7d5
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-22 22:33:54 +0700

    select all locations

--- employee-frontend/src/app/location/location.service.ts
+++ employee-frontend/src/app/location/location.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { Location } from "./location.model";
 
 @Injectable()
 export class LocationService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  get(): Observable<Location[]> {
+    let url = "/api/locations/all"
+
+    return this.http.get(url)
+      .map(response => response.json());
+  }
 }
