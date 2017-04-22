8703abf6ee41640f24414d31b3a8796e29a9984d
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-22 18:34:43 +0700

    change navbar color

--- employee-frontend/src/app/app.module.ts
+++ employee-frontend/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { AppComponent } from './app.component';
 import { EmployeeService } from "./employee/employee.service";
 import { LocationService } from "./location/location.service";
 import { BtnSvgComponent } from './btn-svg/btn-svg.component';
-import { EmployeeSearchComponent } from './employee/employee-search/employee-search.component';
 import { SideTabComponent } from './side-tab/side-tab.component';
 
 
@@ -24,7 +23,6 @@ import { SideTabComponent } from './side-tab/side-tab.component';
     EmployeeListComponent,
     EmployeeDetailComponent,
     BtnSvgComponent,
-    EmployeeSearchComponent,
     SideTabComponent
   ],
   imports: [
