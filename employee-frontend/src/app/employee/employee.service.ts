796ecd225989cf1bffe67d3fd6b4b476cdf7f4d3
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-22 22:33:42 +0700

    add post method

--- employee-frontend/src/app/employee/employee.service.ts
+++ employee-frontend/src/app/employee/employee.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 import { Employee } from './employee.model';
@@ -10,12 +10,22 @@ import { Employee } from './employee.model';
 export class EmployeeService {
 
   constructor(private http: Http) {
-    
+
   }
 
   get(): Observable<Employee[]> {
     let url = "/api/employees/all";
     return this.http.get(url)
+      .map(response => response.json());
+  }
+
+  post(employee): Observable<Employee> {
+    let body = JSON.stringify(employee);
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    let url = "/api/employees/all";
+
+    return this.http.post(url, body, options)
       .map(response => response.json());
   }
 
