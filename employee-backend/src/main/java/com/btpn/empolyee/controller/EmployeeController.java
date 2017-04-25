8e325943c1c14cc5655857eae0f5954a0ac77c63
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-25 14:56:16 +0700

    add more method

--- employee-backend/src/main/java/com/btpn/empolyee/controller/EmployeeController.java
+++ employee-backend/src/main/java/com/btpn/empolyee/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.btpn.empolyee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,33 +25,69 @@ public class EmployeeController {
 	@GetMapping("/employees/all")
 	@ResponseStatus(HttpStatus.OK)
 	public Iterable<Employee> allEmployees() {
-
-		return employeeRepo.findAll();
+		Sort.Order sorting = new Sort.Order(Sort.Direction.ASC, "lastName").ignoreCase();
+		return employeeRepo.findAll(new Sort(sorting));
 	}
-	
+
+	@GetMapping("/employees/getby")
+	@ResponseStatus(HttpStatus.OK)
+	public Iterable<Employee> getBy(@RequestParam String firstName, @RequestParam String lastName,
+			@RequestParam String gender, @RequestParam String location, @RequestParam String sort) {
+		
+		Sort.Order sorting;
+		if (sort.equals("asc")) {
+			
+			sorting = new Sort.Order(Sort.Direction.ASC, "lastName").ignoreCase();
+			
+		} else {
+			
+			sorting = new Sort.Order(Sort.Direction.DESC, "lastName").ignoreCase();
+			
+		}
+		if (gender.equals("") && location.equals("")) {
+			
+			return employeeRepo.findByFirstNameContainingOrLastNameContainingAllIgnoreCase(firstName, lastName,
+					new Sort(sorting));
+			
+		} else if (gender.equals("") && !location.equals("")) {
+			
+			return employeeRepo.findByLocationAndSort(firstName.toUpperCase(), lastName.toUpperCase(),
+					location.toUpperCase(), new Sort(sorting));
+			
+		} else if (!gender.equals("") && location.equals("")) {
+			
+			return employeeRepo.findByGenderAndSort(firstName.toUpperCase(), lastName.toUpperCase(),
+					gender.toUpperCase(), new Sort(sorting));
+			
+		} else {
+			return employeeRepo.findByLocationGenderAndSort(firstName.toUpperCase(), lastName.toUpperCase(),
+					gender.toUpperCase(), location.toUpperCase(), new Sort(sorting));
+		}
+	}
+
 	@PostMapping("/employees/")
 	@ResponseStatus(HttpStatus.CREATED)
-	public void saveEmployee(@RequestBody Employee emp){
+	public void saveEmployee(@RequestBody Employee emp) {
 		employeeRepo.save(emp);
 	}
-	
+
 	@GetMapping("/employees/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public Employee findById (@PathVariable String id){
+	public Employee findById(@PathVariable String id) {
 		return employeeRepo.findOne(id);
 	}
-	
+
 	@PutMapping("/employees/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public void updateEmployee(@PathVariable("id") String id, @RequestBody Employee emp ){
+	public void updateEmployee(@PathVariable String id, @RequestBody Employee emp) {
 		emp.setEmpId(id);
 		employeeRepo.save(emp);
 	}
-	
+
 	@DeleteMapping("/employees/{id}")
 	@ResponseStatus(HttpStatus.OK)
-	public void deleteEmployee(@PathVariable("id") String id){
-		employeeRepo.delete(id);
+	public String deleteEmployee(@PathVariable String id) {
+		return employeeRepo.deleteByEmpId(id);
 	}
 
 }
--- employee-backend/src/main/java/com/btpn/empolyee/repository/EmployeeRepository.java
+++ employee-backend/src/main/java/com/btpn/empolyee/repository/EmployeeRepository.java
@@ -1,10 +1,35 @@
 package com.btpn.empolyee.repository;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.btpn.empolyee.entity.Employee;
 
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, String> {
+
+	public Iterable<Employee> findByFirstNameContainingOrLastNameContainingAllIgnoreCase(
+			@Param("firstName") String firstName, @Param("lastName") String lastName, @Param(value = "asc") Sort sort);
+
+	@Query("select e from Employee e where (UPPER(e.lastName) like %:lastName% or UPPER(e.firstName) like %:firstName%)"
+			+ "and UPPER(e.gender)=:gender")
+	public Iterable<Employee> findByGenderAndSort(@Param("firstName") String firstName,
+			@Param("lastName") String lastName, @Param("gender") String gender, @Param(value = "asc") Sort sort);
+
+	@Query("select e from Employee e where (UPPER(e.lastName) like %:lastName% or UPPER(e.firstName) like %:firstName%)"
+			+ "and UPPER(e.location.id)=:location")
+	public Iterable<Employee> findByLocationAndSort(@Param("firstName") String firstName,
+			@Param("lastName") String lastName, @Param("location") String location, @Param(value = "asc") Sort sort);
+
+	@Query("select e from Employee e where (UPPER(e.lastName) like %:lastName% or UPPER(e.firstName) like %:firstName%)"
+			+ "and UPPER(e.gender)=:gender and UPPER(e.location.id)=:location")
+	public Iterable<Employee> findByLocationGenderAndSort(@Param("firstName") String firstName,
+			@Param("lastName") String lastName, @Param("gender") String gender, @Param("location") String location,
+			@Param(value = "asc") Sort sort);
+
+	@Transactional
+	public String deleteByEmpId(String empId);
 
 }
