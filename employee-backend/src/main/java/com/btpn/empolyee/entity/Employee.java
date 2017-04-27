c396e1df3fd54c74adb5ebfa15729b40868e7e70
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-27 13:05:47 +0700

    add type="text" in photo

--- employee-backend/src/main/java/com/btpn/empolyee/entity/Employee.java
+++ employee-backend/src/main/java/com/btpn/empolyee/entity/Employee.java
@@ -7,12 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -85,7 +87,9 @@ public class Employee {
 	@Column(length = 20, nullable = false)
 	private String status;
 	
+	
 	@Column(nullable = true)
+	@Type(type="text")
 	private String photo;
 	
 	@NotNull
